6d90aaa61f6968d027d6187f23400c0bf802d342
Deer <nomoreiloveyous.m83@gmail.com>
2022-12-24 12:58:41 +0300

    initial

---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m > n) System.Console.WriteLine(Rec(m, n));
+else System.Console.WriteLine(Rec(n, m));
+
+int Rec(int m, int n)
+{
+    if (m == n) return m;
+    return (m + Rec(m - 1, n));
+}
---
+++ task68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Write("Enter m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(Acc(m, n));
+
+int Acc (int m, int n)
+{
+    while (m!= 0)
+    {
+        if (n ==0) n  = 1;
+        else n = Acc(m, n-1);
+        m = m-1;
+    }
+    return n+1;
+}
